95f0bfdb8157d7bb70cd380c8b362c3035878139
Michal Jurek <jurmcc@gmail.com>
2024-10-13 17:48:22 +0200

    Client code. First try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut entities = Entities::new();
+
+    let mut entity1 = Entity::new();
+    entity1.add_component(Componenet::Position);
+    entity1.add_component(Componenet::GeomCircle);
+
+    let mut entity2 = Entity::new();
+    entity2.add_component(Componenet::Position);
+    entity2.add_component(Componenet::GeomSquare);
+
+    entities.add(entity1);
+    entities.add(entity2);
+
+    println!("first iteration--------------");
+    SystemPhysics(&entities);
+    SystemRender(&entities);
+
+    println!("2nd iteration--------------");
+    SystemPhysics(&entities);
+    SystemRender(&entities);
 }
