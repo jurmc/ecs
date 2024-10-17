c7151db49cdac7a914065d1052b5ca7d4ea784db
Michal Jurek <jurmcc@gmail.com>
2024-10-17 22:02:02 +0200

    Before System type parametrization

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
+use std::collections::HashSet;
+
+type SignatureT = u32;
+
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+struct ComponentManager {
+}
+
+impl ComponentManager {
+    fn get_position(&self, _s: SignatureT) ->Vec2 {
+        Vec2 {x: 555.0, y: 666.0}
+    }
+}
+
+struct System {
+    entities: HashSet<SignatureT>,
+    //component_manager:
+
+}
+
+impl System {
+    fn new() -> System {
+        System {entities: HashSet::new()}
+    }
+
+    fn add(&mut self, entity: SignatureT) {
+        self.entities.insert(entity);
+    }
+
+    fn apply(&self, cm: &ComponentManager) {
+        for i in self.entities.iter() {
+            let pos = cm.get_position(*i); // TODO: will be get_component, once we have parametrized
+                                           // component type
+            println!("i: {}, pos.x: {}, pos.y: {}", i, pos.x, pos.y);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! System creation.");
 
-    let mut entities = Entities::new();
+    let cm = ComponentManager {};
+    let mut system = System::new();
+    system.add(1u32);
+    system.add(15u32);
 
-    let mut entity1 = Entity::new();
-    entity1.add_component(Componenet::Position);
-    entity1.add_component(Componenet::GeomCircle);
-
-    let mut entity2 = Entity::new();
-    entity2.add_component(Componenet::Position);
-    entity2.add_component(Componenet::GeomSquare);
-
-    entities.add(entity1);
-    entities.add(entity2);
-
-    println!("first iteration--------------");
-    SystemPhysics(&entities);
-    SystemRender(&entities);
-
-    println!("2nd iteration--------------");
-    SystemPhysics(&entities);
-    SystemRender(&entities);
+    system.apply(&cm);
 }
