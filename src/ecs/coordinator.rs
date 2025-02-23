d77fce88109f7a7269067819a7222863cc709382
Michal Jurek <jurmcc@gmail.com>
2025-02-23 16:09:24 +0100

    WIP: before allowing to add entity to system throug system manager

--- src/ecs/coordinator.rs
+++ src/ecs/coordinator.rs
@@ -37,6 +37,12 @@ impl Coordinator {
 
     pub fn add_component<T: Display + Any>(&mut self, e: Entity, c: T) {
         self.cm.add::<T>(e, c);
+        // TODO: after adding component we need to update system ids in registered systems too
+        println!("To which systems this entity has to be added? e: {}", e);
+        // TODO: 
+        //for s in self.sm.iter() {
+        //    println!("System: {:?}:", s);
+        //}
     }
 
     pub fn get_component<T: Display + Any>(&mut self, e: &Entity) -> Option<&mut T> {
@@ -78,6 +84,7 @@ mod tests {
 
     impl System for SystemU32{
         fn add(&mut self, e: Entity) {
+            println!("System32: add: e: {}", e);
             self.entities.insert(e);
         }
 
@@ -90,7 +97,9 @@ mod tests {
         }
 
         fn apply(&self, cm: &mut ComponentManager) {
+            println!("System32: apply");
             for e in self.entities.iter() {
+                println!("System32: apply for e: {}", e);
                 let v = cm.get::<u32>(e).unwrap();
                 *v += 1;
             }
@@ -101,20 +110,27 @@ mod tests {
     fn test_coordinator_for_simple_component() {
         let mut c = Coordinator::new();
 
+        let s = SystemU32::new();
+        let sys_id = c.register_system(s);
+
         let e1 = c.get_entity();
         let e2 = c.get_entity();
 
-        c.register_component::<i32>();
-        c.add_component(e1, 1);
-        c.add_component(e2, 1);
+        c.register_component::<u32>();
+        let mut v1: u32 = 1;
+        let mut v2: u32 = 1;
+        c.add_component(e1, v1);
+        c.add_component(e2, v2);
 
-        let s = SystemU32::new();
-        let sys_id = c.register_system(s);
+//        let s = SystemU32::new();
+//        let sys_id = c.register_system(s);
         c.apply(&sys_id);
 
 // TODO: point of focus
-// TODO: get this component (afte being updated) and check update
-        //let c1 = c.get
+//        let v1_updated = c.get_component::<u32>(&e1);
+//        assert_eq!(Some(&mut (v1+1)), v1_updated);
+//        let v2_updated = c.get_component::<u32>(&e2);
+//        assert_eq!(Some(&mut (v2+1)), v2_updated);
     }
 }
 
