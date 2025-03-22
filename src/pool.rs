c587f1d5b1ba29343a88a21d785391c814eb9e37
Michal Jurek <jurmcc@gmail.com>
2025-03-22 13:04:35 +0100

    Entities taken from pool can be iterated over (immutably only)

--- src/pool.rs
+++ src/pool.rs
@@ -2,35 +2,42 @@ use crate::MAX_ENTITIES;
 use crate::Entity;
 
 use std::collections::HashSet;
+use std::collections::hash_set::Iter;
 
 pub struct EntitiesPool {
-    available_entities: HashSet<Entity>,
-    used_entities: HashSet<Entity>,
+    available: HashSet<Entity>,
+    taken: HashSet<Entity>,
 }
 
 impl EntitiesPool {
     pub fn new() -> EntitiesPool {
-        let mut available_entities = HashSet::with_capacity(MAX_ENTITIES as usize);
+        let mut available= HashSet::with_capacity(MAX_ENTITIES as usize);
         for e in (0..MAX_ENTITIES).rev() {
-            available_entities.insert(e);
+            available.insert(e);
         }
-        let used_entities: HashSet<Entity> = HashSet::with_capacity(MAX_ENTITIES as usize);
+        let taken: HashSet<Entity> = HashSet::with_capacity(MAX_ENTITIES as usize);
 
-        EntitiesPool { available_entities, used_entities }
+        EntitiesPool { available, taken }
     }
 
-    pub fn get(&mut self) -> Entity { // TODO: this should rather be called 'take'
-        let e = self.available_entities.iter().next().unwrap().clone();
-        self.available_entities.remove(&e);
-        self.used_entities.insert(e);
+    // TODO: will become: take
+    pub fn get(&mut self) -> Entity { // TODO: more like take, since we take something from pool
+                                      // and only can take it back with separate call to giveback
+        let e = self.available.iter().next().unwrap().clone();
+        self.available.remove(&e);
+        self.taken.insert(e);
         e
     }
 
+    // TODO: maybe renamed to: return
     pub fn give_back(&mut self, e: Entity) {
-        self.available_entities.insert(e);
-        self.used_entities.remove(&e);
+        self.available.insert(e);
+        self.taken.remove(&e);
     }
 
+    pub fn taken_iter(&self) -> Iter<'_, Entity> {
+        self.taken.iter()
+    }
 }
 
 #[cfg(test)]
@@ -47,5 +54,21 @@ mod tests {
         let last_e = pool.get();
         pool.give_back(last_e);
         assert_eq!(last_e, pool.get());
+    }
+
+    #[test]
+    fn test_pool_iter() {
+        let mut ep = EntitiesPool::new();
+        let mut taken: HashSet<Entity> = HashSet::new();
+        for _ in 0..10 {
+            taken.insert(ep.get());
+        }
+
+        let mut expected: HashSet<Entity> = HashSet::new();
+        for e in ep.taken_iter() {
+            expected.insert(e.clone());
+        }
+
+        assert_eq!(expected, taken);
     }
 }
