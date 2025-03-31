897f1e8631868873798e9d2e54fe9b00a3debe22
Michal Jurek <jurmcc@gmail.com>
2025-03-31 23:02:15 +0200

    Systems are stored as Rc<RefCell>, so we can have some handles to them outside of ECS

--- src/coordinator.rs
+++ src/coordinator.rs
@@ -9,6 +9,8 @@ use crate::SystemType;
 
 use std::collections::hash_set::Iter;
 use std::any::Any;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 pub struct Coordinator {
     pool: EntitiesPool,
@@ -59,16 +61,16 @@ impl Coordinator {
     }
 
     // Systems
-    pub fn register_system<T: System + Any>(&mut self, s: T) -> SystemType {
-        self.sm.register(s)
+    pub fn register_system<T: System + Any>(&mut self, s: Rc<RefCell<T>>) {
+        self.sm.register(s);
     }
 
-    pub fn apply(&mut self, sys_id: &SystemType) -> Box<dyn Fn(&mut Coordinator)> {
-        self.sm.apply(&sys_id, &mut self.cm)
-    }
-
-    pub fn apply_all(&mut self) -> Vec<Box<dyn Fn(&mut Coordinator)>> {
-        self.sm.apply_all(&mut self.cm)
+    //pub fn apply_all(&mut self) -> Vec<Box<dyn Fn(&mut Coordinator)>> { // TODO: change name to
+    pub fn apply_all(&mut self) { // TODO: change name to just 'apply'
+        let updates = self.sm.apply_all(&mut self.cm);
+        for update in updates {
+            update(self);
+        }
     }
 }
 
@@ -118,11 +120,13 @@ mod tests {
     }
 
     #[test]
-    fn test_system_returning_closure() {
+    fn test_system_returning_closure() { // TODO: returned closures are not externally accesible,
+                                         // maybe whole update should be appliead just by executing
+                                         // closure after system.apply is done?
         let mut c = Coordinator::new();
 
-        let s = SimpleSystem::new(); // TODO: rename SimpleSystem to just TestSystem
-        let sys_id = c.register_system(s);
+        let s = Rc::new(RefCell::new(SimpleSystem::new())); // TODO: rename SimpleSystem to just TestSystem
+        c.register_system(s.clone());
 
         c.register_component::<u32>();
         let e1: u32 = c.entity_take();
@@ -131,29 +135,24 @@ mod tests {
 
         // Our system is expected to:
         // 1) instanlty update e1's v1 comonent
-        // 2) send us update, which after execution
+        // 2) create closure, which after execution
         //   will cause addition of new entity with u32 component
-        let updates = c.apply(&sys_id);
+        c.apply_all();
 
         let expected_e2: u32 = 50;
-        let mut expected_v2: u32 = 100;
+        let expected_v2: u32 = 100;
 
         // Check 1). e1:c1 updated, no additional entity in the ECS
         assert_eq!(Some(&(v1+1)), c.get::<u32>(&e1));
-        assert_eq!(None, c.get::<u32>(&expected_e2));
-
-        // Check 2). aditional component added
-        updates(&mut c);
         assert_eq!(Some(&expected_v2), c.get::<u32>(&expected_e2));
-
     }
 
     #[test]
     fn test_coordinator_for_simple_component() {
         let mut c = Coordinator::new();
 
-        let s = SimpleSystem::new();
-        let sys_id = c.register_system(s); // TODO: this test should work also if system is
+        let s = Rc::new(RefCell::new(SimpleSystem::new()));
+        c.register_system(s); // TODO: this test should work also if system is
                                                    // registered after componets are added (see
                                                    // TODO: below)
 
@@ -166,39 +165,15 @@ mod tests {
         c.add_component(e1, v1);
         c.add_component(e2, v2);
 
-        //let sys_id = c.register_system(s); // TODO: this test should work also if system is
+        //c.register_system(s); // TODO: this test should work also if system is
         //                                           // registered after componets are added (see
         //                                           // TODO: above)
 
-        let _ignored = c.apply(&sys_id); // TODO: _ignored?
+        c.apply_all();
 
         assert_eq!(Some(&(v1+1)), c.get::<u32>(&e1));
         assert_eq!(Some(&(v2+1)), c.get::<u32>(&e2));
     }
-
-    // TODO: we need to have some access to original implementation of registered system
-//    #[test]
-//    fn test_returning_entity_to_pool() {
-//        let mut c = Coordinator::new();
-//
-//        let s = SimpleSystem::new();
-//        let sys_id = c.register_system(s);
-//
-//        let e1 = c.entity_take();
-//        let e2 = c.entity_take();
-//
-//        c.register_component::<u32>();
-//        let v1: u32 = 1;
-//        let v2: u32 = 1;
-//        c.add_component(e1, v1);
-//        c.add_component(e2, v2);
-//
-//        c.entity_back(e1);
-//        assert_eq!(HashSet::from_iter(vec![e2]), s.entities);
-//        // TODO: 1) e1 removed from system
-//        // TODO: 2) e1 components removed from component manager
-//        // TODO: 3) e1 gave back to pool (si it is missing from 'taken' list/set
-//    }
 
     #[derive(Debug, PartialEq)]
     struct Position { x: i32, y: i32, }
@@ -255,8 +230,8 @@ mod tests {
     fn test_coordinator_for_complex_two_componets() {
         let mut c = Coordinator::new();
 
-        let s = ComplexSystem::new();
-        let sys_id = c.register_system(s);
+        let s = Rc::new(RefCell::new(ComplexSystem::new()));
+        c.register_system(s);
 
         let e1 = c.entity_take();
         let e2 = c.entity_take();
@@ -273,7 +248,7 @@ mod tests {
         c.add_component(e2, pos2);
         c.add_component(e2, vel2);
 
-        let _ignored = c.apply(&sys_id);
+        c.apply_all();
 
         let expected_pos1 = &Position { x: 2, y: 4 };
         assert_eq!(expected_pos1, c.get::<Position>(&e1).unwrap());
